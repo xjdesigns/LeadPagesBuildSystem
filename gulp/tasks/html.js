e03b19e53165e3d96fcd3459c33d2ecb7f462b3a
Mon Lu <mon@ave81.com>
2015-02-10 11:35:00 -0600

    Added empty less task file

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,14 +1,11 @@
 var gulp = require('gulp'),
 	minify = require('gulp-minify-html'),
-	connect = require('gulp-connect');
+	connect = require('gulp-connect'),
+	open = require('gulp-open');
 
 gulp.task('html', function () {
   var opts = {comments:true,spare:true};
 
-  // gulp.src('./html/index.html')
-  // 	.pipe(minify(opts))
-  // 	.pipe(gulp.dest('./leadpages-template'))
-
   gulp.src('./leadpages-template/index.html')
-    .pipe(connect.reload());
+    .pipe(connect.reload())
 });
